678e47aa1772b1044901f76bda7df9de71e6a2ad
Isak Lindgren <isak@lindgren.se>
2025-11-26 05:03:05 +0100

    Error Handling
    Added some error handling and descriptions for command args

--- EchoBot.py
+++ EchoBot.py
@@ -6,28 +6,6 @@ from dotenv import load_dotenv
 import os
 import json
 
-load_dotenv()
-
-echoToken = os.environ['ECHO_TOKEN']
-headers = {
-    'Authorization': echoToken,
-}
-
-def gen_echo():
-    response = requests.get('https://api.echo.ac/v1/user/pin', headers=headers).json()
-    return response['links']['dayz']
-
-def get_scan(pin,ScanNumber):
-    try:
-        uuid = requests.get(f'https://api.echo.ac/v1/scan/{pin}', headers=headers).json()[ScanNumber]['uuid']
-        response = requests.get(f'https://api.echo.ac/v1/scan/{uuid}', headers=headers).json()
-        for item in response:
-            print(item)
-        return response
-    except Exception as e:
-        print(type(e))
-        return e
-
 whatIsEchoMessage = """What is Echo?
 Echo is a security tool we use to help verify the integrity of a player’s setup during specific checks or investigations. It’s not an in-game monitoring system — it's used externally by our team when needed.
 
@@ -46,6 +24,30 @@ We use Echo to maintain a fair and competitive environment. It helps us avoid fa
 Player Rights & Transparency
 Echo is only used with your consent and for legitimate checks. You always have the right to decline, though refusal may affect the outcome of an investigation. If you ever feel something was done unfairly, you're welcome to submit a ticket and ask for a review by our lead team."""
 
+
+load_dotenv()
+
+echoToken = os.environ['ECHO_TOKEN']
+headers = {
+    'Authorization': echoToken,
+}
+
+def gen_echo():
+    response = requests.get('https://api.echo.ac/v1/user/pin', headers=headers).json()
+    return response['links']['dayz']
+
+def get_scan(pin,ScanNumber):
+    try:
+        response = requests.get(f'https://api.echo.ac/v1/scan/{pin}', headers=headers)
+        if response.status_code == 204:
+            return 204
+        uuid = response.json()[ScanNumber]['uuid']
+        response = requests.get(f'https://api.echo.ac/v1/scan/{uuid}', headers=headers).json()
+        for item in response:
+            print(item)
+        return response
+    except Exception as e:
+        return e
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -69,16 +71,24 @@ async def gen_echo_link_command(interaction):
     await interaction.response.send_message(f'{whatIsEchoMessage}\n\nPlease download and run this scan then post the does here.\nDownload link: {echoLink}')
 
 @tree.command(name="scan", description="Gets the link for a scan done by given pin", guild=guild)
+@app_commands.describe(pin = "PIN for the scan", scan_number = "Use to access specific scan done with PIN, default: latest scan (-1)")
 async def get_scan_command(interaction: discord.Interaction, pin: str, scan_number: int = -1):
     scanResults = get_scan(pin, scan_number)
+    if scanResults == 204:
+        await interaction.response.send_message(f'Scan with PIN: {pin} not found')
+        return
     try:
         await interaction.response.send_message(f'Scan results: {scanResults["detection"]}\nhttps://beta.dash.echo.ac/scan/{scanResults["uuid"]}')
     except Exception as e:
         await interaction.response.send_message(f'Unexpected error: {e}')
 
 @tree.command(name="details", description="Gets a json file containing the details of a scan by given pin", guild=guild)
+@app_commands.describe(pin = "PIN for the scan", scan_number = "Use to access specific scan done with PIN, default: latest scan (-1)")
 async def get_scan_details_command(interaction: discord.Interaction, pin: str, scan_number: int = -1):
     scanResults = get_scan(pin, scan_number)
+    if scanResults == 204:
+        await interaction.response.send_message(f'Scan with PIN: {pin} not found')
+        return
     with open(f'scan_{pin}_{scan_number}.json', 'w') as outfile:
         json.dump(scanResults, outfile)
     try:
